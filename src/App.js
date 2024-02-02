6641f1e0aa4d122a5d565a3d4b68281810e8ccae
om <om22rajgure@gmail.com>
2024-02-03 01:25:12 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { useState } from "react";
+import XModal from "./components/XModal/XModal";
+
+function App() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [modalOpenBackground, setModalOpenBackground] = useState(false);
+
+  const modalBackground = {
+    height: "100vh",
+    backdropFilter: modalOpenBackground ? "blur(1px)" : "none", // Apply backdrop-filter conditionally
+    transition: "backdrop-filter 0.3s ease", // Add a smooth transition effect
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+
+  return (
+    <div style={modalBackground}>
+      <h1>User Details Modal</h1>
+      <button
+        className="modalTrigger"
+        onClick={() => {
+          setIsOpenModal(true);
+          setModalOpenBackground(true);
+        }}
+      >
+        Open Form
+      </button>
+      {isOpenModal && (
+        <XModal
+          setIsOpenModal={setIsOpenModal}
+          setModalOpenBackground={setModalOpenBackground}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
